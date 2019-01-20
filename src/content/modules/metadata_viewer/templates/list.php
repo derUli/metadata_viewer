6d59b96943f56fd3d8ec1670b113d26fb3f07a03
Ulrich Schmidt <admin@deruli.de>
2019-01-20 12:38:40 +0100

    add icons

--- src/content/modules/metadata_viewer/templates/list.php
+++ src/content/modules/metadata_viewer/templates/list.php
@@ -8,7 +8,7 @@
     if (file_exists($file)) {
         ?>
 	<li><a
-		href="<?php echo ModuleHelper::buildActionURL("show_metadata", "sClass=MetadataViewer&sMethod=show&module=".Template::getEscape($module));?>"><?php Template::escape($module);?></a></li>
+		href="<?php echo ModuleHelper::buildActionURL("show_metadata", "sClass=MetadataViewer&sMethod=show&module=".Template::getEscape($module));?>"><i class="fas fa-eye"></i> <?php Template::escape($module);?></a></li>
 	<?php }?>
 <?php }?>
 </ol>
@@ -22,7 +22,7 @@
     if (file_exists($file)) {
         ?>
 	<li><a
-		href="<?php echo ModuleHelper::buildActionURL("show_metadata", "sClass=MetadataViewer&sMethod=show&theme=".Template::getEscape($theme));?>"><?php Template::escape($theme);?></a></li>
+		href="<?php echo ModuleHelper::buildActionURL("show_metadata", "sClass=MetadataViewer&sMethod=show&theme=".Template::getEscape($theme));?>"><i class="fas fa-eye"></i> <?php Template::escape($theme);?></a></li>
 	<?php }?>
 <?php }?>
 </ol>
--- src/content/modules/metadata_viewer/templates/show.php
+++ src/content/modules/metadata_viewer/templates/show.php
@@ -3,7 +3,7 @@ $acl = new ACL();
 if ($acl->hasPermission(getModuleMeta("metadata_viewer", "admin_permission")) and ViewBag::get("title") and ViewBag::get("content")) {
     ?><p>
 	<a href="<?php echo ModuleHelper::buildAdminURL("metadata_viewer");?>"
-		class="btn btn-default"><?php translate("back");?></a>
+		class="btn btn-default"><i class="fa fa-arrow-left"></i> <?php translate("back");?></a>
 </p>
 <h3><?php Template::escape(ViewBag::get("title"));?></h3>
 <textarea class="codemirror" data-mimetype="application/json" readonly><?php Template::escape(ViewBag::get("content"));?></textarea>
